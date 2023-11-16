63b5f56f4d5f7313416705d20721b33cab655450
suvam <suvampravu@gmail.com>
2023-11-16 19:57:00 +0530

    Basic setup done

---
+++ app.js
@@ -0,0 +1,15 @@
+const express =require('express');
+const mongoose=require('mongoose');
+const ejs=require('ejs');
+
+const app = express();
+
+
+
+
+
+
+
+app.listen('3000',()=>{
+    console.log("App is running at the port 3000");
+})
