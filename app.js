63abbcf102234135483bb5732b6a2ca433227abe
suvam <suvampravu@gmail.com>
2023-11-19 19:18:38 +0530

    basic setup done

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+//Express intialization
 const express =require('express');
+//mongoose intialzation
 const mongoose=require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/WanderLust');
+//EJS intialization
 const ejs=require('ejs');
 
+//seting app with express and view engine
 const app = express();
+app.set('view engine', 'ejs');
 
 
-
+app.get('/',(req,res)=>{
+    res.sendStatus(200);
+})
 
 
 
