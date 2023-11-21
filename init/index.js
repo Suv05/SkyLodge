10e34c60ccdcb3a1645d1fb4b863e730a650ddec
suvam <suvampravu@gmail.com>
2023-11-21 13:22:33 +0530

    indexjs is now ready to insert data to database

--- init/index.js
+++ init/index.js
@@ -1,5 +1,29 @@
-const mongooes=require('mongoose');
+const mongoose = require("mongoose");
 //import listing module
-const Listing=require('./Models/Listing')
-//imoprt data file 
-const data=require('./data');
+const Listing = require("../Models/Listing");
+//imoprt data file
+const initData = require("./data");
+
+//conect to mongodb....
+mongoose.connect("mongodb://127.0.0.1:27017/WanderLust").then(() => {
+  console.log("Sucessfuly connected to dataBase");
+});
+
+//insert Sample data to our website
+const initDB = async () => {
+    try {
+      // Clean existing data from the database
+      await Listing.deleteMany({});
+  
+      // Insert data to the database
+      await Listing.insertMany(initData.data); // Use `initData.data` to get the array of documents
+      console.log("Successfully inserted");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      // Close the connection after inserting data
+      mongoose.connection.close();
+    }
+  };
+  
+  initDB();
