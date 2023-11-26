1cf0f4151e1c304ae5ef0c2383c859caca8b3816
suvam <suvampravu@gmail.com>
2023-11-26 07:15:59 +0530

    show route added

--- app.js
+++ app.js
@@ -1,40 +1,52 @@
 //import listing module
-const Listing=require('./Models/Listing')
-
+const Listing = require("./Models/Listing");
 
 //Express intialization
-const express =require('express');
+const express = require("express");
+//intialize body-parser
+const bodyParser = require("body-parser");
 //mongoose intialzation
-const mongoose=require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/WanderLust');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/WanderLust");
 //EJS intialization
-const ejs=require('ejs');
+const ejs = require("ejs");
+const path = require("path");
 
-//seting app with express and view engine
+//setting the app to express
 const app = express();
-app.set('view engine', 'ejs');
+//now set view engine and public css and also set path
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+//Index route
+app.get("/listings", async (req, res) => {
+  const allListing = await Listing.find({});
+  res.render("listings/index.ejs", { allListing });
+});
 
-app.get('/',(req,res)=>{
-    res.sendStatus(200);
-})
+app.get("/listings/:id",async(req, res) => {
+  let {id} = req.params;
+  const lists= await Listing.findById(id);
+  res.render('listings/show.ejs',{lists});
 
-app.get('/testlisting',async(req,res)=>{
-    const samplelisting=new Listing({
-        title:"Ladvore House La dehi",
-        description:"You wont regereat here",
-        price:255,
-        country:"india",
-        location:"bhubaneswar"
-    })
+});
 
-    await samplelisting.save();
-    console.log("Susecfuly save to db");
-    res.send("Done")
-})
+app.listen("3000", () => {
+  console.log("App is running at the port 3000");
+});
 
+// app.get('/testlisting',async(req,res)=>{
+//     const samplelisting=new Listing({
+//         title:"Ladvore House La dehi",
+//         description:"You wont regereat here",
+//         price:255,
+//         country:"india",
+//         location:"bhubaneswar"
+//     })
 
-
-app.listen('3000',()=>{
-    console.log("App is running at the port 3000");
-})
+//     await samplelisting.save();
+//     console.log("Susecfuly save to db");
+//     res.send("Done")
+// })
